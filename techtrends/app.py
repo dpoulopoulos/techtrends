ef7bd639ec49caf45ce79ef9f94d713c1506fca6
Dimitris Poulopoulos <dpoulopoulos@outlook.com>
2021-06-08 14:33:16 +0000

    Add logs to the application
    Signed-off-by: Dimitris Poulopoulos <dpoulopoulos@outlook.com>

--- techtrends/app.py
+++ techtrends/app.py
@@ -1,104 +1,127 @@
+from werkzeug.exceptions import abort
+import logging
 import sqlite3
 
-from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
-from werkzeug.exceptions import abort
+from flask import (Flask, json, render_template, request,
+                   url_for, redirect, flash)
+
 
 conn_counter = 0
+
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     global conn_counter
 
-    connection = sqlite3.connect('database.db')
+    connection = sqlite3.connect("database.db")
     connection.row_factory = sqlite3.Row
     conn_counter += 1
     return connection
 
+
 # Function to get all posts in the database
 def get_posts():
     connection = get_db_connection()
-    posts = connection.execute('SELECT * FROM posts').fetchall()
+    posts = connection.execute("SELECT * FROM posts").fetchall()
     connection.close()
     return posts
+
 
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
-    post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+    post = connection.execute("SELECT * FROM posts WHERE id = ?",
+                              (post_id,)).fetchone()
     connection.close()
     return post
 
+
 # Define the Flask application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
+app.config["SECRET_KEY"] = "your secret key"
 
-# Define the main route of the web application 
-@app.route('/')
+
+# Define the main route of the web application
+@app.route("/")
 def index():
     posts = get_posts()
-    return render_template('index.html', posts=posts)
+    return render_template("index.html", posts=posts)
 
-# Define how each individual article is rendered 
+
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
-@app.route('/<int:post_id>')
+@app.route("/<int:post_id>")
 def post(post_id):
     post = get_post(post_id)
+
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.info(f"Article ID {post_id} not found. Returning 404.")
+        return render_template("404.html"), 404
     else:
-      return render_template('post.html', post=post)
+        app.logger.info(f"Article '{post['title']}' was retrieved.")
+        return render_template("post.html", post=post)
+
 
 # Define the About Us page
-@app.route('/about')
+@app.route("/about")
 def about():
-     return render_template('about.html')
+    app.logger.info("'About Us' page was retrieved.")
 
-# Define the post creation functionality 
-@app.route('/create', methods=('GET', 'POST'))
+    return render_template("about.html")
+
+
+# Define the post creation functionality
+@app.route("/create", methods=("GET", "POST"))
 def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
 
         if not title:
-            flash('Title is required!')
+            flash("Title is required!")
         else:
             connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            connection.execute("INSERT INTO posts (title, content)"
+                               " VALUES (?, ?)", (title, content))
             connection.commit()
             connection.close()
 
-            return redirect(url_for('index'))
+            app.logger.info(f"Article '{title}' was created.")
 
-    return render_template('create.html')
+            return redirect(url_for("index"))
+
+    return render_template("create.html")
+
 
 # Define the healthcheck endpoint
-@app.route('/healthz', methods=('GET',))
+@app.route("/healthz", methods=("GET",))
 def healthz():
     response = app.response_class(
-        response=json.dumps({'result': 'OK - healthy'}),
+        response=json.dumps({"result": "OK - healthy"}),
         status=200,
-        mimetype='application/json'
+        mimetype="application/json"
     )
     return response
 
+
 # Define the metrics endpoint
-@app.route('/metrics', methods=('GET',))
+@app.route("/metrics", methods=("GET",))
 def metrics():
     global conn_counter
 
     posts = len(get_posts())
     response = app.response_class(
-        response=json.dumps({'db_connection_count': conn_counter,
-                             'post_count': posts}),
+        response=json.dumps({"db_connection_count": conn_counter,
+                             "post_count": posts}),
         status=200,
-        mimetype='application/json'
+        mimetype="application/json"
     )
     return response
 
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(level=logging.DEBUG,
+        format="[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
+    app.run(host="0.0.0.0", port="3111")
